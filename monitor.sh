b86b63b56bb82deccf296e68f25799f03d117b3a
cheshenglin <chesheng@gmail.com>
2019-11-06 15:02:42 +0800

    [ADD] enhance saslauthd test and restart process

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 CRITERIA_ZOMBIE='\[saslauthd\] <defunct>'
+HOSTNAME='openldap'
 RSYSLOG_SERVER=''
+
+TESTSASLAUTH_USER=''
+TESTSASLAUTH_PASSWORD=''
+TESTSASLAUTH_REALM=''
+TESTSASLAUTH_RESULT='/var/log/testsaslauthd.result'
+
+function execute_testsaslauthd() {
+    /usr/sbin/testsaslauthd -u $TESTSASLAUTH_USER -r $TESTSASLAUTH_REALM -p $TESTSASLAUTH_PASSWORD -f /var/run/saslauthd/mux  > $TESTSASLAUTH_RESULT &
+    sleep 3
+    local pid=$(ps aux | grep testsaslauthd | grep mux | awk '{print $2}')
+    if [ -n "$pid" ]; then
+        kill -9 $pid
+    fi
+}
 
 function get_saslauthd_pid() {
     local pid_file=/var/run/saslauthd/saslauthd.pid
@@ -23,15 +38,16 @@ function saslauthd() {
         return 0
     fi
     local zombies_num=$(ps aux | grep "${CRITERIA_ZOMBIE}" | wc -l)
-    local message="<14>ixq-ldap $name[$pid] - $zombies_num zombie processes found."
+    local message="<14>$HOSTNAME $name[$pid] - $zombies_num zombie processes found."
     printf "$message" | nc -u $RSYSLOG_SERVER 514
-    if [ "$zombies_num" -ge 3 ]; then
+
+    local is_valid=$(validate_saslauthd ${zombies_num})
+    if [ $is_valid -eq 0 ]; then
         /usr/bin/systemctl stop $name
-        return 0
     fi
 }
 
-function slapd() {
+slapd() {
     local name="slapd"
     local pid=$(get_slapd_pid)
     if [ -z "$pid" ]; then
@@ -47,6 +63,23 @@ function slapd() {
     local time_wait=$(printf "$connections" | grep 'TIME_WAIT' | wc -l)
     local total=$(printf "$connections" | wc -l)
 
-    local message="<14>ixq-ldap $name[$pid] - $listen/$established/$close_wait/$time_wait/$total (L/E/CW/TW/Total) connections."
+    local message="<14>$HOSTNAME $name[$pid] - $listen/$established/$close_wait/$time_wait/$total (L/E/CW/TW/Total) connections."
     printf "$message" | nc -u $RSYSLOG_SERVER 514
 }
+
+function validate_saslauthd() {
+    args=("$@")
+    local zombies_num=${args[0]}
+    if [ "$zombies_num" -ge 3 ]; then
+        echo 0; return 0
+    fi
+    $(execute_testsaslauthd)
+    local success_match=$(cat $TESTSASLAUTH_RESULT 2>/dev/null | grep -o 'Success' | wc -c)
+    if [ $success_match -eq 8 ]; then
+        echo 1; return 0
+    fi
+    echo 0;
+}
+
+saslauthd
+slapd
